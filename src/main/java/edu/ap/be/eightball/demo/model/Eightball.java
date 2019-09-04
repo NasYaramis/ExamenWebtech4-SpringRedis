8265515da4c21cde27ed54ed7817f4f941219e57
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-09-04 15:19:30 +0200

    added model for eightball

---
+++ src/main/java/edu/ap/be/eightball/demo/model/Eightball.java
@@ -0,0 +1,37 @@
+package edu.ap.be.eightball.demo.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Eightball{
+    private String question;
+    private String answer;
+    private List<String> answers = Arrays.asList("It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.",
+    "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy", "try again", "Ask again later.", "Better not tell you now.",
+    "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no",
+    "Outlook not so good.", "Very doubtful.");
+
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        Random rand = new Random();
+        String random = this.answers.get(rand.nextInt(this.answers.size()));
+        setAnswer(random);
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
